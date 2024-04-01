40e3ef32bbda5d74cc56c68eece5ca4542d19e07
Thonesh77 <thonesh77@example.com>
2024-04-01 21:17:12 +0530

    comment changes

--- smallNumber.java
+++ smallNumber.java
@@ -2,17 +2,16 @@ public class smallNumber {
     public static void main(String[] args) {
         // Array of numbers
         int[] numbers = { 100, 5, 88, 45, 33, 77, 1 };
-        // Initialize maxNumber with the first element of the array
+        // Initialize smallNumber with the first element of the array
         int smallNumber = numbers[0];
-
-        // Iterate through the array to find the maximum number
+        // Iterate through the array to find the small number
         for (int i = 1; i < numbers.length; i++) {
             if (numbers[i] < smallNumber) {
                 smallNumber = numbers[i];
             }
         }
 
-        // Print the maximum number
+        // Print the small number
         System.out.println("The small number is: " + smallNumber);
     }
 }
